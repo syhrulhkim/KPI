a6c7a5109126716536a577220e17acdb0d63c80a
hakimhambali <hakimhambali77@gmail.com>
2021-10-07 23:16:30 +0800

    11 commit 7.10.2021

--- resources/views/livewire/laravel-examples/user-management-form-wire.blade.php
+++ resources/views/livewire/laravel-examples/user-management-form-wire.blade.php
@@ -43,7 +43,7 @@
                                 <label class="control-label" style="font-weight:500">Role</label>
                                 <select wire:model="role" name="role" id="role" class="form-control custom-select" data-placeholder="Choose a Role" tabindex="1">
                                     <option value="">-- Choose a Role --</option>
-                                    <option value="admin">Admin</option>
+                                    {{-- <option value="admin">Admin</option> --}}
                                     <option value="manager">Moderator</option>
                                     <option value="manager">Manager</option>
                                     <option value="hr">HR</option>
