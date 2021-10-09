f0c5154c10f7b547152885b5fa1a0f2b8aca6088
hakimhambali <hakimhambali77@gmail.com>
2021-10-09 10:46:35 +0800

    13 commit 9.10.2021

---
+++ app/Http/Livewire/DashboardManager.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+
+class DashboardManager extends Component
+{
+    public function render()
+    {
+        $userdepartment = auth()->user()->department;
+        // dd($userdepartment);
+        $users = User::where([['department', '=', $userdepartment] , ['role', '=', 'employee']])->orderBy('created_at','desc')->get();
+        // $leaderboards = Leaderboard::where([[ 'id_users', '=', auth()->user()->id] ,[ 'id_createquizzes', '=', $this->id_createquizzes ]])->get();
+        // $users = User::where('role', 'manager')->orWhere('role', 'hr')->orWhere('role', 'moderaor')->get();
+        $userscount = $users->count();
+
+        return view('livewire.dashboard-manager')->with(compact('users', 'userscount'));
+    }
+}
--- resources/views/layouts/navbars/guest/sign-up.blade.php
+++ resources/views/layouts/navbars/guest/sign-up.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent mt-4">
     <div class="container">
-        <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white" href="{{ route('dashboard') }}">
+        <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white" href="{{ route('firstpage') }}">
             Soft UI Dashboard
         </a>
         <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
@@ -17,7 +17,7 @@
                 @if (auth()->user())
                     <li class="nav-item">
                         <a class="nav-link text-white d-flex align-items-center me-2 active" aria-current="page"
-                            href="{{ route('dashboard') }}">
+                            href="{{ route('firstpage') }}">
                             <i class="fa fa-chart-pie opacity-6  me-1"></i>
                             Dashboard
                         </a>
