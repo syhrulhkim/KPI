7ef0096489f4a93aabf222cd590bf766ddd73b12
hakimhambali <hakimhambali77@gmail.com>
2022-01-06 21:24:01 +0800

    67 commit 6.1.2022

--- resources/views/livewire/date2.blade.php
+++ resources/views/livewire/date2.blade.php
@@ -37,7 +37,7 @@
                     @endforeach
                   </div>
                   <div class="card-body px-0 pt-0 pb-2">
-                    <div class="table-responsive p-0">
+                    <div class="p-0">
                       <table class="table align-items-center mb-0">
                         <thead>
                           <tr>
