dace274fc44f45f9a82a495dedaf389f6f957af8
syhrulhkim <89004999+syhrulhkim@users.noreply.github.com>
2022-01-07 09:45:50 +0800

    Latest Design 7.1.22 9:45AM

--- resources/views/livewire/laravel-examples/user-management-form-wire.blade.php
+++ resources/views/livewire/laravel-examples/user-management-form-wire.blade.php
@@ -69,7 +69,7 @@
                             {{-- <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
                                 Save
                             </button> --}}
-                            <button type="submit" class="btn btn-success btn-sm" style="font-size: 10px"><i class="fas fa-save"></i> Save</button>
+                            <button type="submit" class="btn bg-gradient-dark mb-0" href="javascript:;"><i class="fas fa-plus"></i> Save</button>
                         </div>
                     </div>
                 </div>
